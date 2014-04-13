258e8ce286b69a6cb177f67a3d56542bd1fdedf6
Kevin L. Mitchell <klmitch@mit.edu>
2014-04-13 09:44:50 -0500

    Make SmartVersion a true int
    SmartVersion was a class that looks like an int, but the only
    differences are the constructor and some methods not defined on
    normal ints.  This means that SmartVersion can actually subclass
    an int.  We have to switch __init__() out for __new__() and
    eliminate the special integer methods, but otherwise it looks the
    same, and now isinstance(version, int) will be True.

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -13,177 +13,25 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import operator
 import unittest
 
 import mock
-import six
 
 from vobj import version
 
 
 class SmartVersionTest(unittest.TestCase):
-    def test_init_basic(self):
+    def test_new_basic(self):
         sv = version.SmartVersion(5, 'schema')
 
-        self.assertEqual(sv._version, 5)
         self.assertEqual(sv._schema, 'schema')
         self.assertEqual(sv._master, None)
 
-    def test_init_master(self):
+    def test_new_master(self):
         sv = version.SmartVersion(5, 'schema', 'master')
 
-        self.assertEqual(sv._version, 5)
         self.assertEqual(sv._schema, 'schema')
         self.assertEqual(sv._master, 'master')
-
-    representations = {
-        repr: repr(25),
-        str: str(25),
-        complex: complex(25),
-        int: int(25),
-        float: float(25),
-    }
-    if six.PY2:
-        representations.update({
-            unicode: unicode(25),
-            long: long(25),
-            oct: oct(25),
-            hex: hex(25),
-        })
-    elif six.PY3:
-        representations.update({
-            round: round(25),
-        })
-
-    def test_representations(self):
-        sv = version.SmartVersion(25, 'schema')
-
-        for type_, expected in self.representations.items():
-            result = type_(sv)
-
-            self.assertEqual(type(expected), type(result))
-            self.assertEqual(expected, result)
-
-    comparisons = [
-        (5, 7, {
-            operator.lt: True,
-            operator.le: True,
-            operator.eq: False,
-            operator.ne: True,
-            operator.gt: False,
-            operator.ge: False,
-        }),
-        (7, 5, {
-            operator.lt: False,
-            operator.le: False,
-            operator.eq: False,
-            operator.ne: True,
-            operator.gt: True,
-            operator.ge: True,
-        }),
-        (5, 5, {
-            operator.lt: False,
-            operator.le: True,
-            operator.eq: True,
-            operator.ne: False,
-            operator.gt: False,
-            operator.ge: True,
-        }),
-    ]
-
-    def test_comparisons_integer(self):
-        for lhs, rhs, truth_tab in self.comparisons:
-            lhs = version.SmartVersion(lhs, 'schema')
-
-            for op, expected in truth_tab.items():
-                self.assertEqual(expected, op(lhs, rhs))
-
-    def test_comparisons_smart_version(self):
-        for lhs, rhs, truth_tab in self.comparisons:
-            lhs = version.SmartVersion(lhs, 'schema')
-            rhs = version.SmartVersion(rhs, 'schema')
-
-            for op, expected in truth_tab.items():
-                self.assertEqual(expected, op(lhs, rhs))
-
-    def test_hash(self):
-        for i in range(100):
-            sv = version.SmartVersion(i, 'schema')
-
-            self.assertEqual(hash(i), hash(sv))
-
-    def test_bool(self):
-        for i in range(100):
-            sv = version.SmartVersion(i, 'schema')
-
-            self.assertEqual(True, bool(sv))
-
-    binary_operators = [
-        operator.add, operator.sub, operator.mul,
-        operator.floordiv, operator.truediv, operator.mod, divmod, pow,
-        operator.lshift, operator.rshift, operator.and_, operator.xor,
-        operator.or_,
-    ]
-    if six.PY2:
-        binary_operators.append(operator.div)
-
-    def test_binary_operators_integer(self):
-        for op in self.binary_operators:
-            expected = op(25, 35)
-            lhs = version.SmartVersion(25, 'schema')
-
-            result = op(lhs, 35)
-
-            self.assertEqual(expected, result)
-
-    def test_binary_operators_smart_version(self):
-        for op in self.binary_operators:
-            expected = op(25, 35)
-            lhs = version.SmartVersion(25, 'schema')
-            rhs = version.SmartVersion(35, 'schema')
-
-            result = op(lhs, rhs)
-
-            self.assertEqual(expected, result)
-
-    def test_binary_operators_reflected(self):
-        for op in self.binary_operators:
-            expected = op(25, 35)
-            rhs = version.SmartVersion(35, 'schema')
-
-            result = op(25, rhs)
-
-            self.assertEqual(expected, result)
-
-    def test_pow_modulo_integer(self):
-        expected = pow(25, 35, 15)
-        lhs = version.SmartVersion(25, 'schema')
-
-        result = pow(lhs, 35, 15)
-
-    def test_pow_modulo_smart_version(self):
-        expected = pow(25, 35, 15)
-        lhs = version.SmartVersion(25, 'schema')
-        rhs = version.SmartVersion(35, 'schema')
-        modulo = version.SmartVersion(15, 'schema')
-
-        result = pow(lhs, rhs, modulo)
-
-        self.assertEqual(expected, result)
-
-    unary_operators = [
-        operator.neg, operator.pos, abs, operator.invert, operator.index,
-    ]
-
-    def test_unary_operators(self):
-        for op in self.unary_operators:
-            expected = op(25)
-            sv = version.SmartVersion(25, 'schema')
-
-            result = op(sv)
-
-            self.assertEqual(expected, result)
 
     def test_len_no_schema(self):
         sv = version.SmartVersion(1, None)
